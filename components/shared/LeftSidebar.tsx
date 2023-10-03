b3e260ee224fcb2df88f6c189e692432c00a3365
escafl0wne <falladvance@outlook.com>
2023-10-03 22:11:59 +0500

    css adjustments

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,32 +1,30 @@
-'use client'
+"use client";
 import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Navlinks from "./Navlinks";
 
 export default function LeftSidebar() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        {" "}
-        <Navlinks position="leftsidebar"/>
+        <Navlinks position="leftsidebar" />
       </div>
       <div className="mt-10 px-6">
-      <SignedIn>
-            <SignOutButton signOutCallback={()=> router.push("/sign-in")}>
-              <div className="flex cursor-pointer gap-4 p-4">
-                <Image
-                  alt="logout"
-                  src="/assets/logout.svg"
-                  width={24}
-                  height={24}
-                />
-                <p className="text-light-2 max-lg:hidden">Logout</p>
-              </div>
-            </SignOutButton>
-          </SignedIn>
-
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                alt="logout"
+                src="/assets/logout.svg"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
--- components/shared/Navlinks.tsx
+++ components/shared/Navlinks.tsx
@@ -14,11 +14,11 @@ type TNavlinkProps={
 export default function Navlinks({position}:TNavlinkProps){
     const pathname = usePathname()
     const pClassName = position === "leftsidebar" ? "text-light-1 max-lg:hidden" : "text-subtle-medium text-light-1 max-sm:hidden"
-    
+    const d = position === "leftsidebar" ? "relative flex justify-start gap-4 rounded-lg p-4" : "relative flex flex-col items-center gap-2 rounded-lg p-2 sm:flex-1 sm:px-2 sm:py-2.5"
     return sidebarLinks.map((link) =>{
         const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
         return  (
-          <Link href={link.route} key={link.label} className={`${position}_link ${isActive && 'bg-primary-500'}`}>
+          <Link href={link.route} key={link.label} className={`${d} ${isActive && 'bg-primary-500'}`}>
           <Image src={link.imgURL} alt={link.label} width={24} height={24} />
           <p className={pClassName}>{ position === "leftsidebar" ? link.label : link.label.split(/\s+/)[0]}</p>
         </Link>
